b1312fc65fd34369b18b6fee36b48405ff0ace8c
Kevin Froman <beardog@mailbox.org>
2021-01-04 17:14:55 -0600

    Added recievehandler stub

--- simplegossip/RoutingBuffers.cs
+++ simplegossip/RoutingBuffers.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 namespace simplegossip{
 
     public interface RecieveHandler{
-        void handleRecieve(byte[] message);
-        void handleSend(byte[] message);
+
+        void handleRecieve(Epidemic epidemic, Peer peer, byte[] message);
+        void handleSend(Epidemic epidemic, Peer peer, byte[] message);
 
     }
 
     public class RoutingBuffer{
 
-
+        RecieveHandler handler;
+        public RoutingBuffer(RecieveHandler handler){
+            this.handler = handler;
+        }
 
     }
 
